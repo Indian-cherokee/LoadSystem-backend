6b5cba0bb70a9d03369051b5e697718c470de03f
Artem Nikiforov <a.nikiforov@INV-2572.local>
2025-10-20 16:28:16 +0300

    fix

--- internal/app/ds/users.go
+++ internal/app/ds/users.go
@@ -3,7 +3,7 @@ package ds
 type Users struct {
 	ID        uint   `gorm:"primaryKey;column:id"`
 	Username  string `gorm:"unique;column:username;size:255;not null"`
-	Password  string `gorm:"unique;column:password;size:255;not null"`
+	Password  string `gorm:"column:password;size:255;not null"`
 	Moderator bool   `gorm:"column:moderator;not null"`
 
 	CalcSessions []LoadSession `gorm:"foreignKey:CreatorID"`
